4a60d5acd980da244b711e35c913e763a91b003c
Cristian Olivares <cris.olivares.90@gmail.com>
2025-08-12 21:51:55 -0400

    primer commit

---
+++ formulario/formulario.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+
+    document.getElementById("formulario-registro")
+        .addEventListener("submit", function (event) {
+            console.log('haciendo submit del formulario');
+
+            event.preventDefault();
+            this.reset();
+        });
+
+
+
+}); 
